7ef923ed69a631e488893e4a53a1eeaf1337c1ca
kareem-khalfalla <kareem.khalfalla.dev@gmail.com>
2021-07-30 14:13:30 +0200

    refactor URL

--- components/Projects.js
+++ components/Projects.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:8088/api/projects";
+const url = "https://kareem-khalfalla.herokuapp.com/api/projects";
 const Projects = async () => {
   const data = await fetch(url);
   const jsonData = await data.json();
