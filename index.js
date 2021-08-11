46b7aacea87923de5ae70ca3c7b27e809640a031
kareem-khalfalla <kareem.khalfalla.dev@gmail.com>
2021-08-11 17:43:25 +0200

    epic edit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import Projects from './components/Projects.js';
 
 document.getElementById('projects').innerHTML = await Projects();
+
+let scroll = document.getElementById('scrollTop');
+addEventListener('scroll', () => {
+    scroll.classList.toggle('active', window.scrollY > 500)
+});
+scroll.addEventListener('click', () => {
+    scrollTo({
+        top: 0
+    });
+});
